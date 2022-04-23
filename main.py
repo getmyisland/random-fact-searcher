6ee56610971008cf200c079d0f2fd10e322d7866
getmyisland <getmyisland@gmail.com>
2022-04-23 10:05:39 +0200

    Added higher sleeptime and google searching

--- main.py
+++ main.py
@@ -10,31 +10,41 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 facts_file = open('facts.txt', 'a')
 
 # Create a new firefox window
-driver = webdriver.Firefox()
+browser = webdriver.Firefox()
 
 # Search for random facts
-driver.get('https://www.google.com/search?client=firefox-b-d&q=i%27m+feeling+curious&ved=2ahUKEwiz9c-C1Kf3AhXGAewKHaJHBMAQ73N6BAgKEA0')
-driver.find_element_by_id('L2AGLb').click()
+browser.get('https://www.google.de/')
+browser.find_element_by_id('L2AGLb').click()
+
+# Find the search bar and search
+search = browser.find_element_by_name('q')
+search.send_keys("i'm feeling curious")
+
+# Hit return after you enter search text
+search.send_keys(Keys.RETURN)
+
+# Sleep for 5 seconds so you can see the results
+time.sleep(5)
 
 # Endless loop to run the program forever
 n = 1
-while n != 5:
+while n != 2:
     # Wait a few seconds to load
-    time.sleep(3)
+    time.sleep(5)
 
     # Get the fact title
-    element_fact_title = driver.find_element_by_class_name("sW6dbe")
+    element_fact_title = browser.find_element_by_class_name("sW6dbe")
     fact_title = element_fact_title.text
 
     # Get the fact
-    element_fact = driver.find_element_by_class_name("DRBylb")
+    element_fact = browser.find_element_by_class_name("DRBylb")
     fact = element_fact.text
 
     # Write the fact to the text file
     facts_file.write('\n' + fact_title + ';' + fact)
 
     # Refresh the page to get a new fact
-    driver.refresh()
+    browser.refresh()
 
-driver.close()
+browser.close()
 facts_file.close()
